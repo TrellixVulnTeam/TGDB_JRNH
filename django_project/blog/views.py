cbdd849da2b35abe58b588647a91f686ce5bf6f5
SeedBoot <oliboots@gmail.com>
2017-10-12 16:02:37 +0100

    next page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 import datetime
+from django import template
 
 def index(request):
     return HttpResponse("<h1>Hello Django</h1>")
 
 def today_is(request):
     now = datetime.datetime.now()
-    html = "<html><body>Current date and time: {0}</body></html>".format(now)
+    t = template.Template("<html><body>Current date and time {{ now }}</body></html>")
+    c = template.Context({'now': now})
+    html = t.render(c)
     return HttpResponse(html)
