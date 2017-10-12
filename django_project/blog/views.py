e21cb71171ecd0935d810e9a56c029b3c7ec0f1b
SeedBoot <oliboots@gmail.com>
2017-10-12 16:18:42 +0100

    lesson

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,10 @@
 from django.http import HttpResponse
 import datetime
-from django import template
+from django.shortcuts import render_to_response
 
 def index(request):
     return HttpResponse("<h1>Hello Django</h1>")
 
 def today_is(request):
     now = datetime.datetime.now()
-    t = template.Template("<html><body>Current date and time {{ now }}</body></html>")
-    c = template.Context({'now': now})
-    html = t.render(c)
-    return HttpResponse(html)
+    return render_to_response('blog/datetime.html', {'now': now})
