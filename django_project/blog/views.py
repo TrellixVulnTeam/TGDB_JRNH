7bae05c938fd598b788b0ba89f708856d94c2f9b
SeedBoot <oliboots@gmail.com>
2017-10-19 09:14:07 +0100

    steps

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
+from django.conf import settings
 import datetime
-from django.shortcuts import render
 
 def index(request):
     return HttpResponse("<h1>Hello Django</h1>")
 
 def today_is(request):
     now = datetime.datetime.now()
-    return render_to_response('blog/datetime.html', {'now': now})
+    return render(request, 'blog/datetime.html', {
+                                    'now': now,
+                                    'template': 'blog/nav.html',
+                                    'base_dir': settings.BASE_DIR
+                                    })
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print("\nBASE_DIR =", BASE_DIR, "\n")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
